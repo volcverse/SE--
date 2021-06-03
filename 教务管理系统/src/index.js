2ced08f69f62f274dabc71dcdc917ef605a2ddf9
Dummy Head <72564247+DummyNodeHead@users.noreply.github.com>
2021-06-03 21:30:38 +0800

    Add files via upload

---
+++ 教务管理系统/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+import {BrowserRouter} from 'react-router-dom'
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App></App>
+  </BrowserRouter>
+  ,
+  document.getElementById('root')
+);
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
