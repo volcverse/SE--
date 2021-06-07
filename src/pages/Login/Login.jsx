0d12f2e454dc4b989ef5b040ad212bd820bf1ea7
Dummy Head <72564247+DummyNodeHead@users.noreply.github.com>
2021-06-07 18:13:21 +0800

    Add files via upload

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,64 +1,25 @@
 import React, { Component } from 'react'
-import { Form, Input, Button, Radio } from 'antd'
-import { Link, Route } from 'react-router-dom'
+import { Route, Redirect, Switch } from 'react-router-dom'
+import ManagerCenter from '../ManagerCenter/ManagerCenter'
+import StudentCenter from '../StudentCenter/StudentCenter'
+import TeacherCenter from '../TeacherCenter/TeacherCenter'
+import LoginInterface from './LoginInterface'
+import ForgetPSW from './ForgetPSW'
 
 
 export default class Login extends Component {
     render() {
         return (
-            <div className="login">
-                <div className="login-content-wrap">
-                    <div className="login-content">
-                        <img className="logo" src="resources/logo.png" />
-
-                        <div className="login-from">
-
-                            <Form.Item>
-                                <Input placeholder="用户名" />
-                            </Form.Item>
-
-                            <Form.Item>
-                                <Input type="password" placeholder="密码" />
-                            </Form.Item>
-                            <Form.Item>
-                                您是：
-                                <Choose></Choose>
-                            </Form.Item>
-                            <Form.Item><Link to="/ManagerCenter">
-                                <Button className="login-form-button" type="primary" shape="round" size='large'>
-                                    登录
-                                    
-                                </Button></Link>
-                            </Form.Item>
-
-                            <Form.Item>
-                                <Button className="login-form-button" type="primary" shape="round" size='large'>
-                                    忘记密码
-                                </Button>
-                            </Form.Item>
-                        </div>
-                    </div>
-                </div>
-                
-            </div>
+                <Switch>
+                    <Route path="/ManagerCenter" component={ManagerCenter}></Route>
+                    <Route path="/StudentCenter" component={StudentCenter}></Route>
+                    <Route path="/TeacherCenter" component={TeacherCenter}></Route>
+                    <Route path="/LoginInterface" component={LoginInterface}></Route>
+                    <Route path="/ForgetPSW" component={ForgetPSW}></Route>
+                    <Redirect to="/LoginInterface"></Redirect>
+                </Switch>
         )
     }
 }
 
 
-const Choose = () => {
-    const [value, setValue] = React.useState(1);
-
-    const onChange = e => {
-        console.log('radio checked', e.target.value);
-        setValue(e.target.value);
-    };
-
-    return (
-        <Radio.Group onChange={onChange} value={value}>
-            <Radio name="choice" value={1}>学生</Radio>
-            <Radio name="choice" value={2}>教师</Radio>
-            <Radio name="choice" value={3}>管理员</Radio>
-        </Radio.Group>
-    );
-};
