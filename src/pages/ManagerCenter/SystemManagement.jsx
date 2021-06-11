0813bf24d54316a11242d0225f37990fc3cf995a
So1rymaker <85490045+So1rymaker@users.noreply.github.com>
2021-06-11 15:22:51 +0800

    Update SystemManagement.jsx

--- src/pages/ManagerCenter/SystemManagement.jsx
+++ src/pages/ManagerCenter/SystemManagement.jsx
@@ -1,40 +1,50 @@
-import { Input, Button, Form } from 'antd';
+import { Input, Button, Form,DatePicker, TimePicker,Col,InputNumber } from 'antd';
 import React from 'react';
 import { Select } from 'antd';
-const { Option } = Select;
+const { RangePicker } = DatePicker;
 
 
-export default class FindCourse extends React.Component {
+export default class SystemManagement extends React.Component {
   render() {
     const formItemLayout = {
       labelCol: {
-        xs: { span: 24 },
+        xs: { span: 24},
         sm: { span: 4, offset: 4 },
       },
       wrapperCol: {
         xs: { span: 24 },
-        sm: { span: 8 },
+        sm: { span: 8},
       },
     };
+	    const formItemLayout1 = {
+      labelCol: {
+        xs: { span: 24},
+        sm: { span: 7, offset: 4 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 2},
+      },
+    };
+
 
     return (
 
       <>
         <br /><br /><br /><br /><br />
-        <Form.Item label="搜索关键字" {...formItemLayout}>
-          <Input></Input>
-        </Form.Item>
-        <Form.Item {...formItemLayout} label="依据">
-          <Select defaultValue="name" onChange={handleChange}>
-            <Option value="name">课程名</Option>
-            <Option value="cID">课程号</Option>
-            <Option value="teacher">任课教师</Option>
-            <Option value="credit">学分</Option>
-          </Select>
-        </Form.Item>
+	 
+      <Form.Item name="PrimarySelectionTime" label="初选时间设置" {...formItemLayout}>
+        <RangePicker showTime format="YYYY-MM-DD HH:mm:ss " />
+      </Form.Item>
+      <Form.Item name="By-electionTime" label="补选时间设置" {...formItemLayout}>
+        <RangePicker showTime format="YYYY-MM-DD HH:mm:ss " />
+      </Form.Item>      
+	  <Form.Item name="studentnum" label="在线人数限制" rules={[{ type: 'number', min: 0, max: 10000 }]}{...formItemLayout1}>
+        <InputNumber/>
+        </Form.Item>	
         <br/>
-        <Button style={{ width: 200 }} type="primary" shape="round" size='large'>
-              查找课程信息
+        <Button style={{ width: 100 }} type="primary" shape="round" size='large'>
+              确定
             </Button>
         <br/>
         
@@ -51,3 +61,7 @@ export default class FindCourse extends React.Component {
 function handleChange(value) {
   console.log(`selected ${value}`);
 }
+
+
+
+
